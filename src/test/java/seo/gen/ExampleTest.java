5faca3a42efd896a75713a9316f95b01c5ff998b
Oxyq <seo-x@mail.ru>
2023-02-07 03:51:06 +0300

    use rnd

--- src/test/java/seo/gen/ExampleTest.java
+++ src/test/java/seo/gen/ExampleTest.java
@@ -3,13 +3,15 @@ package seo.gen;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
     @Test
     void exampleTest0() {
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(new Random().nextInt() >1);
     }
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(11>3);
+        Assertions.assertTrue(new Random().nextInt()>3);
     }
 }
